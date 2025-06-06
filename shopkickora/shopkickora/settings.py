bb7f29774ce1b983b6a3fcb1d01a270024a21d3e
prithviraj <prithvirajpu999@gmail.com>
2025-06-06 22:55:00 +0530

    protected the id and private details

--- shopkickora/shopkickora/settings.py
+++ shopkickora/shopkickora/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,12 +147,14 @@ SOCIALACCOUNT_PROVIDERS = {
 
 
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pythondevelopment10@gmail.com'
-EMAIL_HOST_PASSWORD = 'hzdqbohnbwoopoos'  # App password, looks correct
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+ # App password, looks correct
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
