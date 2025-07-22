bc3ffdf1e5182237b3e81922e2c8cbff0361102a
prithviraj <prithvirajpu999@gmail.com>
2025-07-22 15:50:02 +0530

    Remove cached .pyc and __pycache__ files

--- shopkickora/user_app/signals.py
+++
@@ -1,9 +0,0 @@
-
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import CustomUser, Wallet
-
-@receiver(post_save, sender=CustomUser)
-def create_wallet(sender, instance, created, **kwargs):
-    if created:
-        Wallet.objects.create(user=instance)
